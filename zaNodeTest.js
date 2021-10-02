5a2bd1b2d9e5e09a72fe48ceb72bc441f0228e84
Josip Balta <josip.balta@gmail.com>
2021-10-02 10:01:05 +0200

    dodao neke stvari

---
+++ zaNodeTest.js
@@ -0,0 +1,14 @@
+console.log("alo node");
+// let res = new Date("2021-04-01");
+// let res = new Date("April 1,2021");
+// let res = new Date("15 June 2020 13:30:00");
+// let res = new Date("15 January 2020 13:30:00");
+// let res = new Date(2020, 0, 5, 13, 30, 45);
+// let res = new Date("2016");
+let res = new Date("2016-07-07T13:30:45");
+//'2021-04-01'
+//"April 1, 2021"
+let utc = "2021-04-01T00:00:00Z";
+let cetWinter = "2021-04-01T00:00:00+0100";
+// let res = new Date(cetWinter);
+console.log(res);
